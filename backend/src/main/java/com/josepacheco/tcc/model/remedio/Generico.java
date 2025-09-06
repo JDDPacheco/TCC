1de7564619510311ed8b9ad951c53412f0d1420c
jddacheco <josediogo.dp@gmail.com>
2025-09-06 13:32:28 -0400

    melhorando heranca de remedio

--- backend/src/main/java/com/josepacheco/tcc/model/remedio/Generico.java
+++ backend/src/main/java/com/josepacheco/tcc/model/remedio/Generico.java
@@ -1,4 +1,10 @@
 package com.josepacheco.tcc.model.remedio;
 
+import jakarta.persistence.*;
+
+@Entity
+@DiscriminatorValue("generico")
 public class Generico extends Remedio{
+    // classe apenas de anotação, sem atributos específicos
+    // vou criar uma regra para que o nome do produto seja a formula quando o remédio for generico
 }
